f8c9797e2612af6363576554ea91348663d1af0b
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-08 09:58:07 +0800

    初始化和修改配置

---
+++ server/core/conf.go
@@ -0,0 +1,40 @@
+package core
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+	"io/fs"
+	"log"
+	"os"
+	"server/config"
+)
+
+const ConfigFile = "settings.yaml"
+
+// InitConf 读取yaml文件的配置
+func InitConf() {
+	c := &config.Config{}
+	yamlConf, err := os.ReadFile(ConfigFile)
+	if err != nil {
+		panic(fmt.Errorf("get yamlConf error: %s", err))
+	}
+	err = yaml.Unmarshal(yamlConf, c)
+	if err != nil {
+		log.Fatalf("config Init Unmarshal: %v", err)
+	}
+	log.Println("config yamlFile load Init success.")
+	global.Config = c
+}
+func SetYaml() error {
+	byteData, err := yaml.Marshal(global.Config)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(ConfigFile, byteData, fs.ModePerm)
+	if err != nil {
+		return err
+	}
+	logrus.Info("配置文件修改成功")
+	return nil
+}
