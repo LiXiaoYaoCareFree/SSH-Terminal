ccc0165991296a2bd7f7290487261e13a9ef0ea1
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-08 10:24:24 +0800

    添加消息结构体及解析功能

---
+++ server/proxy/sshd/dto/message.go
@@ -0,0 +1,38 @@
+package dto
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type Message struct {
+	Type    int `json:"type"`
+	Content any `json:"content"`
+}
+
+func (r Message) ToString() string {
+	byteData, _ := json.Marshal(r)
+	return string(byteData)
+}
+
+func NewMessage(_type int, content string) Message {
+	return Message{Content: content, Type: _type}
+}
+
+func ParseMessage(value string) (message Message, err error) {
+	if value == "" {
+		return
+	}
+	_type, err := strconv.Atoi(value[:1])
+	if err != nil {
+		return
+	}
+	var content = value[1:]
+	message = NewMessage(_type, content)
+	return
+}
+
+type WindowSize struct {
+	Cols int `json:"cols"`
+	Rows int `json:"rows"`
+}
